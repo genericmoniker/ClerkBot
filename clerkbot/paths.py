18b6b3c3e086e1d400c828042448938bb90bd376
Eric Smith <eric@esmithy.net>
2018-12-29 10:07:08 -0700

    Allow the config directory to come from the environment.

--- clerkbot/paths.py
+++ clerkbot/paths.py
@@ -1,6 +1,7 @@
+import os
 from pathlib import Path
 
-CONF_DIR = Path('/conf')  # Docker
+CONF_DIR = Path(os.environ.get('CONF_DIR', '/conf'))  # /conf for Docker
 if not CONF_DIR.exists():
     CONF_DIR = Path.home() / 'clerkbot' / 'conf'
 
